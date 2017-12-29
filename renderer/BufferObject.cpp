455c999f4185cacbb2e7a3ee04b23699e89e6ba9
Holger Frydrych <holger@frydrych.org>
2017-12-29 16:41:45 +0100

    Use correct preprocessor flag for SSE2 support

--- renderer/BufferObject.cpp
+++ renderer/BufferObject.cpp
@@ -45,7 +45,7 @@ void UnbindBufferObjects() {
 CopyBuffer
 ========================
 */
-#ifdef ID_WIN_X86_SSE2_INTRIN
+#ifdef __SSE2__
 typedef unsigned int uint32;
 void CopyBuffer( byte * dst, const byte * src, int numBytes ) {
 	assert_16_byte_aligned( dst );
