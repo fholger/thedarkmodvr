0b43548e14f7cd72c3938a92ddaad6f7f26cabeb
Holger Frydrych <holger@frydrych.org>
2018-01-05 06:16:57 +0100

    Disable glColor to make nSight work

--- renderer/draw_common.cpp
+++ renderer/draw_common.cpp
@@ -523,10 +523,10 @@ void RB_STD_T_RenderShaderPasses_OldStage( idDrawVert *ac, const shaderStage_t *
 		cubeMapShader.Use();
 		break;
 	case TG_REFLECT_CUBE:
-		qglColor4fv(color);
+		//qglColor4fv(color);
 		break;
 	case TG_SCREEN:
-		qglColor4fv( color );
+		//qglColor4fv( color );
 	default:
 		qglEnableVertexAttribArray( 8 );
 		qglVertexAttribPointer( 8, 2, GL_FLOAT, false, sizeof( idDrawVert ), ac->st.ToFloatPtr() );
@@ -698,7 +698,7 @@ void RB_STD_T_RenderShaderPasses_SoftParticle( idDrawVert *ac, const shaderStage
 		color[1] = regs[pStage->color.registers[1]];
 		color[2] = regs[pStage->color.registers[2]];
 		color[3] = regs[pStage->color.registers[3]];
-		qglColor4fv( color );
+		//qglColor4fv( color );
 	} else
 	{
 		// A properly set-up particle shader
