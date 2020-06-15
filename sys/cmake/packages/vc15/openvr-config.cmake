bf1835f1da35c2f2eb4c7f090f67a8e106bfa6d7
Holger Frydrych <holger@frydrych.org>
2020-06-15 22:45:46 +0200

    Add OpenVR as a thirdparty artefact

---
+++ sys/cmake/packages/vc15/openvr-config.cmake
@@ -0,0 +1,4 @@
+set(OPENVR_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openvr/include")
+set(OPENVR_LIBRARY_DIR "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openvr/lib/win64")
+set(OPENVR_LIBRARIES "${OPENVR_LIBRARY_DIR}/openvr_api.lib")
+set(OPENVR_DLL "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openvr/bin/win64/openvr_api.dll")
