53d2fa1f15ce527506ce6ba7607dcab0a50f327a
Holger Frydrych <holger@frydrych.org>
2020-06-27 00:06:37 +0200

    Add OpenXR to dependencies

---
+++ sys/cmake/packages/vc15/openxr-config.cmake
@@ -0,0 +1,3 @@
+set(OPENXR_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openxr/include")
+set(OPENXR_LIBRARY_DIR "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openxr/lib/${PACKAGE_PLATFORM}")
+set(OPENXR_LIBRARIES "${OPENXR_LIBRARY_DIR}/openxr_loader.lib")
