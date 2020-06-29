edb6705b83ec2f4587babc0df0392534919f4ceb
Holger Frydrych <holger@frydrych.org>
2020-06-29 21:54:38 +0200

    wip Initialize OpenXR

--- renderer/vr/VrBackend.cpp
+++ renderer/vr/VrBackend.cpp
@@ -14,21 +14,23 @@
 ******************************************************************************/
 #include "precompiled.h"
 #include "VrBackend.h"
-#include <openxr/openxr_platform.h>
+#include "xr_loader.h"
+#include "../../sys/win32/win_local.h"
 
 VrBackend vrImpl;
 VrBackend *vr = &vrImpl;
 
 namespace {
-	void CheckXrResult( XrInstance instance, XrResult result ) {
-		if ( XR_SUCCEEDED( result ) ) {
-			return;
-		}
-
-		char resultString[XR_MAX_RESULT_STRING_SIZE];
-		xrResultToString( instance, result, resultString );
-		common->FatalError( "Failed to initialize OpenXR: %s", resultString );
-	}
+#ifdef WIN32
+XrGraphicsBindingOpenGLWin32KHR Sys_CreateGraphicsBinding() {
+	return {
+		XR_TYPE_GRAPHICS_BINDING_OPENGL_WIN32_KHR,
+		nullptr,
+		win32.hDC,
+		win32.hGLRC,
+	};
+}
+#endif
 }
 
 void VrBackend::Init() {
@@ -36,39 +38,52 @@ void VrBackend::Init() {
 		Destroy();
 	}
 
+	common->Printf( "-----------------------------\n" );
 	common->Printf( "Initializing VR subsystem\n" );
 
-	const char * const enabledExtensions[] = { XR_KHR_OPENGL_ENABLE_EXTENSION_NAME };
-	XrInstanceCreateInfo instanceCreateInfo = {
-		XR_TYPE_INSTANCE_CREATE_INFO,
+	instance = XR_CreateInstance();
+	XrSystemGetInfo systemGetInfo = {
+		XR_TYPE_SYSTEM_GET_INFO,
 		nullptr,
-		0,
-		{
-			"The Dark Mod",
-			209,
-			"",
-			0,
-			XR_CURRENT_API_VERSION,
-		},
-		0,
-		nullptr,
-		1,
-		enabledExtensions,
+		XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY,
 	};
-	XrResult result = xrCreateInstance( &instanceCreateInfo, &instance );
-	CheckXrResult( instance, result );
-	common->Printf( "...Created OpenXR instance\n" );
+	XrResult result = xrGetSystem( instance, &systemGetInfo, &system );
+	XR_CheckResult( result, "acquiring the system id", instance );
 
-	XrInstanceProperties instanceProperties = {
-		XR_TYPE_INSTANCE_PROPERTIES,
+	// must call graphicsRequirements before opening a session, although the results are not
+	// that interesting for us
+	XrGraphicsRequirementsOpenGLKHR openglReqs = {
+		XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_KHR,
 		nullptr,
 	};
-	result = xrGetInstanceProperties( instance, &instanceProperties );
-	CheckXrResult( instance, result );
-	common->Printf( "...OpenXR runtime: %s (v%d.%d.%d)\n", instanceProperties.runtimeName,
-		XR_VERSION_MAJOR( instanceProperties.runtimeVersion ),
-		XR_VERSION_MINOR( instanceProperties.runtimeVersion ),
-		XR_VERSION_PATCH( instanceProperties.runtimeVersion ) );
+	result = xrGetOpenGLGraphicsRequirementsKHR( instance, system, &openglReqs );
+	XR_CheckResult( result, "calling OpenGL graphics requirements", instance );
+
+	auto graphicsBinding = Sys_CreateGraphicsBinding();
+	XrSessionCreateInfo sessionCreateInfo = {
+		XR_TYPE_SESSION_CREATE_INFO,
+		&graphicsBinding,
+		0,
+		system,
+	};
+	result = xrCreateSession( instance, &sessionCreateInfo, &session );
+	XR_CheckResult( result, "creating session", instance );
+	common->Printf( "Session created\n" );
+
+	uint32_t numSupportedFormats = 0;
+	result = xrEnumerateSwapchainFormats( session, 0, &numSupportedFormats, nullptr );
+	XR_CheckResult( result, "getting supported swapchain formats", instance );
+	idList<int64_t> swapchainFormats;
+	swapchainFormats.SetNum( numSupportedFormats );
+	result = xrEnumerateSwapchainFormats( session, swapchainFormats.Num(), &numSupportedFormats, swapchainFormats.Ptr() );
+	XR_CheckResult( result, "getting supported swapchain formats", instance );
+	common->Printf("Supported swapchain formats: ");
+	for ( int64_t format : swapchainFormats ) {
+		common->Printf( "%d ", format );
+	}
+	common->Printf( "\n" );
+
+	common->Printf( "-----------------------------\n" );
 }
 
 void VrBackend::Destroy() {
@@ -76,6 +91,66 @@ void VrBackend::Destroy() {
 		return;
 	}
 
+	xrDestroySession( session );
+	session = nullptr;
+
 	xrDestroyInstance( instance );
 	instance = nullptr;
+}
+
+void VrBackend::BeginFrame() {
+	// poll xr events and react to them
+	XrEventDataBuffer event = {
+		XR_TYPE_EVENT_DATA_BUFFER,
+		nullptr,
+	};
+	XrResult result = xrPollEvent( instance, &event );
+	XR_CheckResult( result, "polling events", instance );
+	if ( result == XR_SUCCESS ) {
+		switch ( event.type ) {
+		case XR_TYPE_EVENT_DATA_SESSION_STATE_CHANGED:
+			HandleSessionStateChange( *reinterpret_cast<XrEventDataSessionStateChanged*>(&event) );
+			break;
+		case XR_TYPE_EVENT_DATA_INSTANCE_LOSS_PENDING:
+			common->FatalError( "xr Instance lost" );
+		}
+	}
+
+	if ( !vrSessionActive ) {
+		return;
+	}
+}
+
+void VrBackend::EndFrame() {}
+
+void VrBackend::HandleSessionStateChange( const XrEventDataSessionStateChanged &stateChangedEvent ) {
+	XrSessionBeginInfo beginInfo = {
+		XR_TYPE_SESSION_BEGIN_INFO,
+		nullptr,
+		XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO,
+	};
+	XrResult result;
+
+	switch ( stateChangedEvent.type ) {
+	case XR_SESSION_STATE_READY:
+		common->Printf( "xr Session is ready, beginning session...\n" );
+		result = xrBeginSession( session, &beginInfo );
+		XR_CheckResult( result, "beginning session", instance );
+		vrSessionActive = true;
+		break;
+
+	case XR_SESSION_STATE_SYNCHRONIZED:
+	case XR_SESSION_STATE_VISIBLE:
+	case XR_SESSION_STATE_FOCUSED:
+		vrSessionActive = true;
+		break;
+
+	case XR_SESSION_STATE_IDLE:
+		vrSessionActive = false;
+		break;
+
+	case XR_SESSION_STATE_LOSS_PENDING:
+	case XR_SESSION_STATE_STOPPING:
+		common->FatalError( "xr Session shutting down or lost" );
+	}	
 }
