5e9b7b1c6b73964ff5b3cb01b31d7e4e6eeb8408
Holger Frydrych <holger@frydrych.org>
2020-07-04 14:44:14 +0200

    Adjust to recent CMake find package changes

--- sys/cmake/packages/vc15/openxr-config.cmake
+++
@@ -1,3 +0,0 @@
-set(OPENXR_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openxr/include")
-set(OPENXR_LIBRARY_DIR "${PROJECT_SOURCE_DIR}/ThirdParty/artefacts/openxr/lib/${PACKAGE_PLATFORM}")
-set(OPENXR_LIBRARIES "${OPENXR_LIBRARY_DIR}/openxr_loader.lib")
