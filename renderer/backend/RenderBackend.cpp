1faa75c949f6669d2af95c83c56f53e4a88a9789
Holger Frydrych <holger@frydrych.org>
2020-07-09 16:41:23 +0200

    Experiment with recalculating light scissors for performance

--- renderer/backend/RenderBackend.cpp
+++ renderer/backend/RenderBackend.cpp
@@ -209,7 +209,7 @@ void RenderBackend::DrawInteractionsWithStencilShadows( const viewDef_t *viewDef
 	if ( vLight->globalShadows || vLight->localShadows ) {
 		backEnd.currentScissor = vLight->scissorRect;
 
-		if ( r_useScissor.GetBool() ) {
+		if ( /*r_useScissor.GetBool()*/ true ) {
 			GL_ScissorVidSize( viewDef->viewport.x1 + backEnd.currentScissor.x1,
 			            viewDef->viewport.y1 + backEnd.currentScissor.y1,
 			            backEnd.currentScissor.x2 + 1 - backEnd.currentScissor.x1,
@@ -250,6 +250,8 @@ void RenderBackend::DrawInteractionsWithStencilShadows( const viewDef_t *viewDef
 	}
 
 	interactionStage.DrawInteractions( vLight, vLight->globalInteractions );
+
+	GL_ScissorRelative( 0, 0, 1, 1 );
 
 	GLSLProgram::Deactivate();
 }
