53348af26ec111c612ce28d6731853faa94b7b2d
Holger Frydrych <holger@frydrych.org>
2020-07-15 18:33:43 +0200

    Use Doom3 BFG MVP projection code for light and shadow scissors

--- renderer/tr_light.cpp
+++ renderer/tr_light.cpp
@@ -733,53 +733,6 @@ stencil clears and interaction drawing
 */
 static int c_clippedLight = 0, c_unclippedLight = 0;
 
-idScreenRect R_WorldBoxToScissor( const idBox &box, viewDef_t *viewDef ) {
-	// transform the bounds into view space
-	idVec3 viewOrigin;
-	R_LocalPointToGlobal( viewDef->worldSpace.modelViewMatrix, box.GetCenter(), viewOrigin );
-	idBounds viewBounds (viewOrigin);
-
-	idVec3 boxPoints[8];
-	idVec3 viewPoints[8];
-	box.ToPoints( boxPoints );
-	for (int i = 0; i < 8; ++i) {
-		R_LocalPointToGlobal( viewDef->worldSpace.modelViewMatrix, boxPoints[i], viewPoints[i] );
-		viewBounds.AddPoint( viewPoints[i] );
-	}
-
-	idVec3 points[8];
-	viewBounds.ToPoints( points );
-
-	idScreenRect scissor;
-	scissor.Clear();
-
-	int width = viewDef->viewport.x2 - viewDef->viewport.x1;
-	int height = viewDef->viewport.y2 - viewDef->viewport.y1;
-	
-	idPlane clip[8];
-	for ( int i = 0; i < 8; ++i ) {
-		R_PointTimesMatrix( viewDef->projectionMatrix, idVec4(points[i].x, points[i].y, points[i].z, 1), clip[i].ToVec4() );
-		R_TransformClipToDevice( clip[i], viewDef, points[i] );
-		points[i].x = 0.5f * (points[i].x + 1) * width;
-		points[i].y = 0.5f * (points[i].y + 1) * height;
-		points[i].z = 0.5f * (points[i].z + 1);
-		scissor.AddPoint( points[i].x, points[i].y );
-	}
-
-	if ( viewBounds.ContainsPoint( idVec3(0, 0, 0) ) ) {
-		// light bounds enclose the camera, so need to return the full scissor
-		scissor = viewDef->scissor;
-	}
-
-	scissor.Intersect( viewDef->scissor );
-	R_TransformEyeZToWin( viewBounds[1].z, viewDef->projectionMatrix, scissor.zmin );
-	R_TransformEyeZToWin( viewBounds[0].z, viewDef->projectionMatrix, scissor.zmax );
-	scissor.zmin = idMath::ClampFloat( 0, 1, scissor.zmin );
-	scissor.zmax = idMath::ClampFloat( scissor.zmin, 1, scissor.zmax );
-
-	return scissor;
-}
-
 idScreenRect R_CalcLightScissorRectangle( viewLight_t *vLight, viewDef_t *viewDef ) {
 	idScreenRect	r;
 	idPlane			eye, clip;
@@ -788,7 +741,6 @@ idScreenRect R_CalcLightScissorRectangle( viewLight_t *vLight, viewDef_t *viewDe
 	if ( vLight->lightDef->parms.pointLight ) {
 		idBounds bounds;
 		idRenderLightLocal *lightDef = vLight->lightDef;
-		return R_WorldBoxToScissor( idBox( lightDef->parms.origin, lightDef->parms.lightRadius, lightDef->parms.axis ), viewDef );
 		viewDef->viewFrustum.ProjectionBounds( idBox( lightDef->parms.origin, lightDef->parms.lightRadius, lightDef->parms.axis ), bounds );
 		return R_ScreenRectFromViewFrustumBounds( bounds, viewDef );
 	}
